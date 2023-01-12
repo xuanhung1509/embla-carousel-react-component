77cf184603d74bc60707c4bb2be3a8a7760a662c
xuanhung1509 <vuxuanhung15092002@gmail.com>
2023-01-12 10:25:13 +0700

    chore: add example usages

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,177 @@
-import useMediaQuery from '@/hooks/useMediaQuery';
 import Carousel from '@/Carousel/Carousel';
 import CarouselSlide from '@/Carousel/CarouselSlide';
-import { PrevButton, NextButton } from '@/Carousel/CarouselButton';
-import {
-  AutoHeight,
-  Autoplay,
-  ClassNames,
-  WheelGesturesPlugin,
-} from '@/Carousel/Plugins';
 import './App.css';
 
-const MyPrevButton = () => (
-  <PrevButton className='absolute top-4 right-16 flex h-10 w-10 items-center justify-center rounded-full bg-slate-300 px-4 py-2'>
-    &lt;
-  </PrevButton>
-);
-
-const MyNextButton = () => (
-  <NextButton className='absolute top-4 right-4 flex h-10 w-10 items-center justify-center rounded-full bg-slate-300 px-4 py-2'>
-    &gt;
-  </NextButton>
-);
-
-const App = () => {
-  const isMD = useMediaQuery('(min-width: 768px)');
-  const isLG = useMediaQuery('(min-width: 1024px)');
-  const slidesPerViewTablet = isMD ? 2 : 1;
-  const slidesPerView = isLG ? 4 : slidesPerViewTablet;
-
-  return (
-    <Carousel
-      slidesPerView={slidesPerView}
-      slideGap='1rem'
-      plugins={[
-        Autoplay({
-          delay: 2000,
-        }),
-        ClassNames({
-          draggable: 'cursor-grab',
-          dragging: 'cursor-grabbing',
-        }),
-        WheelGesturesPlugin(),
-        AutoHeight(),
-      ]}
-      PrevButton={MyPrevButton}
-      NextButton={MyNextButton}
-      className='relative mx-auto mt-8 max-w-5xl rounded-lg px-4 pb-4 pt-20'
-    >
-      {[...Array(17).keys()].map((n) => (
-        <CarouselSlide key={n} className='h-40 last:h-72'>
-          <div className='h-full w-full rounded-lg bg-slate-300 p-4'>
+const Default = () => (
+  <div>
+    <h2 className='text-2xl font-bold'>Default</h2>
+    <Carousel slideGap='1rem' slidesPerView={1} className='mt-4'>
+      {[...Array(5).keys()].map((n) => (
+        <CarouselSlide key={n}>
+          <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
         </CarouselSlide>
       ))}
     </Carousel>
-  );
-};
+  </div>
+);
+
+const RightToLeft = () => (
+  <div>
+    <h2 className='text-2xl font-bold'>Right To Left</h2>
+    <Carousel
+      slideGap='1rem'
+      slidesPerView={1}
+      options={{
+        direction: 'rtl',
+      }}
+      className='mt-4'
+      style={{
+        direction: 'rtl',
+      }}
+    >
+      {[...Array(5).keys()].map((n) => (
+        <CarouselSlide key={n}>
+          <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
+            Slide {n + 1}
+          </div>
+        </CarouselSlide>
+      ))}
+    </Carousel>
+  </div>
+);
+
+const SlidesToScroll = () => (
+  <div>
+    <h2 className='text-2xl font-bold'>Slides To Scroll</h2>
+    <Carousel
+      slideGap='1rem'
+      slidesPerView={2}
+      options={{
+        slidesToScroll: 2,
+      }}
+      className='mt-4'
+    >
+      {[...Array(8).keys()].map((n) => (
+        <CarouselSlide key={n}>
+          <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
+            Slide {n + 1}
+          </div>
+        </CarouselSlide>
+      ))}
+    </Carousel>
+  </div>
+);
+
+const DragFree = () => (
+  <div>
+    <h2 className='text-2xl font-bold'>Drag Free</h2>
+    <Carousel
+      slideGap='1rem'
+      slidesPerView={2}
+      options={{
+        slidesToScroll: 2,
+        dragFree: true,
+      }}
+      className='mt-4'
+    >
+      {[...Array(8).keys()].map((n) => (
+        <CarouselSlide key={n}>
+          <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
+            Slide {n + 1}
+          </div>
+        </CarouselSlide>
+      ))}
+    </Carousel>
+  </div>
+);
+
+const Align = () => (
+  <div>
+    <h2 className='text-2xl font-bold'>Align</h2>
+    <Carousel
+      slideGap='1rem'
+      slidesPerView={1.25}
+      options={{
+        align: 'center',
+      }}
+      className='mt-4'
+    >
+      {[...Array(5).keys()].map((n) => (
+        <CarouselSlide key={n}>
+          <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
+            Slide {n + 1}
+          </div>
+        </CarouselSlide>
+      ))}
+    </Carousel>
+  </div>
+);
+
+const VariableWidths = () => (
+  <div>
+    <h2 className='text-2xl font-bold'>Variable Widths</h2>
+    <Carousel
+      slideGap='1rem'
+      slidesPerView={1}
+      containerStyle={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+      options={{
+        align: 'center',
+      }}
+      className='mt-4'
+    >
+      {[...Array(5).keys()].map((n) => (
+        <CarouselSlide key={n} className='embla-slide'>
+          <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
+            Slide {n + 1}
+          </div>
+        </CarouselSlide>
+      ))}
+    </Carousel>
+  </div>
+);
+
+const YAxis = () => (
+  <div>
+    <h2 className='text-2xl font-bold'>Y Axis</h2>
+    <Carousel
+      slideGap='1rem'
+      slidesPerView={1}
+      containerStyle={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '11rem',
+      }}
+      options={{
+        axis: 'y',
+      }}
+      className='mt-4'
+    >
+      {[...Array(5).keys()].map((n) => (
+        <CarouselSlide key={n} className='embla-slide'>
+          <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
+            Slide {n + 1}
+          </div>
+        </CarouselSlide>
+      ))}
+    </Carousel>
+  </div>
+);
+
+const App = () => (
+  <div className='container mx-auto flex flex-col items-stretch gap-8 py-6 px-4'>
+    <Default />
+    <RightToLeft />
+    <SlidesToScroll />
+    <DragFree />
+    <Align />
+    <VariableWidths />
+    <YAxis />
+  </div>
+);
 
 export default App;
