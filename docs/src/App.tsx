f175ad591357a8901802cc4dbf33593718319941
xuanhung1509 <vuxuanhung15092002@gmail.com>
2023-01-30 10:34:25 +0700

    fix: namespace components

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,12 +1,4 @@
-import {
-  Carousel,
-  CarouselSlide,
-  PrevButton,
-  NextButton,
-  Dots,
-  Thumbs,
-  Thumb,
-} from 'embla-carousel-react-component';
+import Carousel from 'embla-carousel-react-component';
 import Autoplay from 'embla-carousel-autoplay';
 import ClassNames from 'embla-carousel-class-names';
 import { WheelGesturesPlugin as WheelGestures } from 'embla-carousel-wheel-gestures';
@@ -17,11 +9,11 @@ const Default = () => (
     <h2 className='text-2xl font-bold'>Default</h2>
     <Carousel perView={1} className='mt-4'>
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -41,11 +33,11 @@ const RightToLeft = () => (
       }}
     >
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -62,11 +54,11 @@ const SlidesToScroll = () => (
       className='mt-4'
     >
       {[...Array(8).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -84,11 +76,11 @@ const DragFree = () => (
       className='mt-4'
     >
       {[...Array(8).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -105,11 +97,11 @@ const Align = () => (
       className='mt-4'
     >
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -129,11 +121,11 @@ const VariableWidths = () => (
       className='mt-4'
     >
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n} className='embla-slide'>
+        <Carousel.Slide key={n} className='embla-slide'>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -154,41 +146,41 @@ const YAxis = () => (
       className='mt-4'
     >
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
 );
 
 const MyPrevButton = () => (
-  <PrevButton
+  <Carousel.PrevButton
     className='absolute top-1/2 left-0 z-10 flex h-12 w-12 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-slate-200 px-4 py-2 shadow'
     disabledClassName='cursor-default opacity-50'
   >
     <svg className='h-1/2 w-1/2' viewBox='137.718 -1.001 366.563 644'>
       <path d='M428.36 12.5c16.67-16.67 43.76-16.67 60.42 0 16.67 16.67 16.67 43.76 0 60.42L241.7 320c148.25 148.24 230.61 230.6 247.08 247.08 16.67 16.66 16.67 43.75 0 60.42-16.67 16.66-43.76 16.67-60.42 0-27.72-27.71-249.45-249.37-277.16-277.08a42.308 42.308 0 0 1-12.48-30.34c0-11.1 4.1-22.05 12.48-30.42C206.63 234.23 400.64 40.21 428.36 12.5z' />
     </svg>
-  </PrevButton>
+  </Carousel.PrevButton>
 );
 
 const MyNextButton = () => (
-  <NextButton
+  <Carousel.NextButton
     className='absolute top-1/2 right-0 z-10 flex h-12 w-12 translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-slate-200 px-4 py-2 shadow'
     disabledClassName='cursor-default opacity-50'
   >
     <svg className='embla__button__svg' viewBox='0 0 238.003 238.003'>
       <path d='M181.776 107.719L78.705 4.648c-6.198-6.198-16.273-6.198-22.47 0s-6.198 16.273 0 22.47l91.883 91.883-91.883 91.883c-6.198 6.198-6.198 16.273 0 22.47s16.273 6.198 22.47 0l103.071-103.039a15.741 15.741 0 0 0 4.64-11.283c0-4.13-1.526-8.199-4.64-11.313z' />
     </svg>
-  </NextButton>
+  </Carousel.NextButton>
 );
 
 const MyDots = () => (
   <div className='absolute bottom-4 left-0 right-0 z-10 flex items-center justify-center gap-2'>
-    <Dots
+    <Carousel.Dots
       className='h-2 w-5 rounded'
       selectedClassName='bg-slate-900'
       nonSelectedClassName='bg-slate-400'
@@ -207,20 +199,20 @@ const ArrowsAndDots = () => (
       className='relative mt-4'
     >
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
 );
 
 const MyThumbs = () => (
-  <Thumbs perView={3.5} gap='0.5rem' className='mt-4'>
+  <Carousel.Thumbs perView={3.5} gap='0.5rem' className='mt-4'>
     {[...Array(5).keys()].map((n) => (
-      <Thumb
+      <Carousel.Thumb
         key={n}
         index={n}
         className='h-24 rounded bg-slate-300'
@@ -228,9 +220,9 @@ const MyThumbs = () => (
         nonSelectedClassName='opacity-50'
       >
         {n + 1}
-      </Thumb>
+      </Carousel.Thumb>
     ))}
-  </Thumbs>
+  </Carousel.Thumbs>
 );
 
 const Thumbnails = () => (
@@ -238,11 +230,11 @@ const Thumbnails = () => (
     <h2 className='text-2xl font-bold'>Thumbnails</h2>
     <Carousel perView={1} Thumbs={MyThumbs} className='mt-4'>
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -261,11 +253,11 @@ const AutoPlayPlugin = () => (
       className='mt-4'
     >
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -285,11 +277,11 @@ const ClassNamesPlugin = () => (
       className='mt-4'
     >
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
@@ -300,11 +292,11 @@ const WheelGesturesPlugin = () => (
     <h2 className='text-2xl font-bold'>Wheel Gestures</h2>
     <Carousel perView={1} plugins={[WheelGestures()]} className='mt-4'>
       {[...Array(5).keys()].map((n) => (
-        <CarouselSlide key={n}>
+        <Carousel.Slide key={n}>
           <div className='h-40 w-full rounded-md bg-slate-300 p-4'>
             Slide {n + 1}
           </div>
-        </CarouselSlide>
+        </Carousel.Slide>
       ))}
     </Carousel>
   </div>
--- packages/embla-carousel-react-component/src/lib/index.ts
+++ packages/embla-carousel-react-component/src/lib/index.ts
@@ -2,6 +2,13 @@ import Carousel from './Carousel';
 import CarouselSlide from './CarouselSlide';
 import { PrevButton, NextButton } from './CarouselButtons';
 import Dots from './CarouselDots';
-import { Thumb, Thumbs } from './CarouselThumbs';
+import { Thumbs, Thumb } from './CarouselThumbs';
 
-export { Carousel, CarouselSlide, PrevButton, NextButton, Dots, Thumb, Thumbs };
+export default Object.assign(Carousel, {
+  Slide: CarouselSlide,
+  PrevButton,
+  NextButton,
+  Dots,
+  Thumbs,
+  Thumb,
+});
