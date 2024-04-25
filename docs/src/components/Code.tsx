a8066209ffb807478410097a0809c1cffebba406
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-04-25 23:25:54 +0700

    fix(docs): migrate to react-syntax-highlighter

--- docs/src/components/Code.tsx
+++ docs/src/components/Code.tsx
@@ -1,12 +1,15 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 interface CodeProps {
   block: any;
   lang: 'tsx' | 'css' | 'sh';
 }
 
 const Code = ({ block, lang }: CodeProps) => (
-  <pre>
-    <code className={`language-${lang}`}>{block}</code>
-  </pre>
+  <SyntaxHighlighter language={lang} style={tomorrow}>
+    {block}
+  </SyntaxHighlighter>
 );
 
 export default Code;
